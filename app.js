a7b40bf2499a8688a7864cf99b70b5eb513e6ab0
Mike Anthony Ygoña <mikeanthony595@gmail.com>
2024-01-16 00:36:24 +0800

    added initial files

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const cors = require("cors");
+
+// Start express app
+const app = express();
+
+app.enable("trust proxy");
+
+// Set security http headers
+app.use(helmet());
+
+// Implement CORS
+app.use(cors());
+app.options("*", cors());
+
+// limit requests from same IP ( 100 request per IP )
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+app.use(compression());
+
+// HTTP request logger
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+module.exports = app;
