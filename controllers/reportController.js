6e8e683cd338fade586e77e412f350ef89e892d8
Mike Anthony Ygoña <mike.ygona@proweaver.net>
2024-01-20 19:35:14 +0800

    added update report status for admins

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,4 +1,5 @@
 const Report = require('../models/reportModel')
+const AppError = require('../utils/appError')
 const catchAsyncErrors = require('../utils/catchAsyncErrors')
 
 exports.createReport = catchAsyncErrors(async (req, res, next) => {
@@ -12,10 +13,29 @@ exports.createReport = catchAsyncErrors(async (req, res, next) => {
 
 exports.getAllReport = catchAsyncErrors(async (req, res, next) => {
   const reports = await Report.find()
-  console.log(reports)
   res.status(200).json({
     message: 'success',
     results: reports.length,
     data: reports,
+  })
+})
+
+exports.updateReportStatus = catchAsyncErrors(async (req, res, next) => {
+  const { reportId, status } = req.query
+  const report = await Report.findByIdAndUpdate(
+    reportId,
+    { status },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+  if (!report) {
+    next(new AppError('No Report found by that report ID', 404))
+  }
+
+  res.status(200).json({
+    message: 'success',
+    report,
   })
 })
